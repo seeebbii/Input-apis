f2a843ae85d8e51352b24e095dfcd183d5b08275
seeebbii <haseebzafar98@gmail.com>
2022-01-21 00:50:31 +0500

    Production bug fixes

--- dist/service/uri.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoUri = "mongodb+srv://input:input@cluster0.e0hib.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-exports.default = mongoUri;
